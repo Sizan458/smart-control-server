0d308816f65ca86a9680efc78388e66e6f4b00cf
Sizan458 <www.shijan23@gmail.com>
2023-11-24 00:09:24 +0600

    depoly to vercel

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
-import { MongoClient, ServerApiVersion } from 'mongodb';
+import { MongoClient, ObjectId, ServerApiVersion } from 'mongodb';
 
 const app = express();
 const port =process.env.PORT ||5001;
@@ -24,6 +24,31 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    //create a new  database collection
+    const ALLApartment=  client.db("smart-build-control-server").collection("all-apartments")
+    //all apartments  databases
+    //insert data into database
+app .post ("/all-apartments", async(req, res) =>{
+    const apartments = req.body;
+    const result = await ALLApartment.insertOne(apartments);
+    res.send(result);
+
+})
+//show all data in sever site
+app.get("/all-apartments", async (req, res) => {
+    const result = await ALLApartment.find().toArray();
+    res.send(result);
+  });
+  //get data by id
+  app.get("/all-apartments/:id",async(req, res) => {
+    const id = req.params.id;
+    const query = {
+        _id: new ObjectId(id),
+      };
+      console.log(query);
+    const result = await ALLApartment.findOne(query);
+    res.send(result);
+  })
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
