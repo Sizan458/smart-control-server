63b7fa4ad0fcef31626081c2e9a6523d8a4951fe
Sizan458 <www.shijan23@gmail.com>
2023-11-23 22:35:24 +0600

    make a server

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+
+const app = express();
+const port =process.env.PORT ||5001;
+// middleware
+app.use(cors());
+app.use(express.json());
+//home page
+app.get('/', (req, res) => {
+    res.send("Welcome to  my server!");
+})
+// exceed the server
+app.listen(port,()=>{console.log(`server is listening on ${port}`)}); 
